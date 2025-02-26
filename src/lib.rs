a820db5f8044f6de7f5cd252f6d910f3ea41fded
Administrator <yiranmushroom@gmail.com>
2025-02-25 22:08:41 -0800

    publish

---
+++ src/lib.rs
@@ -0,0 +1,21 @@
+pub use proc_macro2;
+pub use quote::quote;
+pub use syn;
+
+pub mod utils {
+    use super::*;
+    use proc_macro_crate::*;
+    fn get_call_site_crate_name(original_name: &str) -> proc_macro2::TokenStream {
+        proc_macro2::TokenStream::from(
+            match crate_name(original_name)
+                .expect(format!("{} must be present in Cargo.toml", original_name).as_str())
+            {
+                FoundCrate::Itself => quote!(#original_name),
+                FoundCrate::Name(name) => {
+                    let ident = syn::Ident::new(&name, proc_macro2::Span::call_site());
+                    quote! {#ident}
+                }
+            },
+        )
+    }
+}
